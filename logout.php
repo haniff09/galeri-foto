6c747ba8057865fb7c55851fb4b3d47ee31c64aa
haniff09 <qowiyhanif@gmail.com>
2025-12-10 08:26:10 +0700

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,5 +1,10 @@
 <?php
+// Memulai session agar bisa diakses dan dihentikan
 session_start();
+
+// Menghapus semua data session (logout user)
 session_destroy();
+
+// Mengarahkan user kembali ke halaman login setelah logout
 header("Location: login.php");
 ?>
