03ed6b2d94ecf6f7bb6e5f5f916db9182c23b705
haniff09 <qowiyhanif@gmail.com>
2025-12-10 08:25:09 +0700

    Update like.php

--- like.php
+++ like.php
@@ -1,26 +1,39 @@
 <?php
+// Memulai session untuk mengakses data user yang sedang login
 session_start();
+
+// Menghubungkan ke database
 include "config.php";
 
+// Mengambil ID foto dari form (hidden input)
 $foto = $_POST['fotoid'];
+
+// Mengambil UserID dari session (user yang sedang login)
 $uid  = $_SESSION['UserID'];
 
+// Mengecek apakah user sudah pernah like foto ini
 $cek = mysqli_query($koneksi,
     "SELECT * FROM likefoto WHERE FotoID='$foto' AND UserID='$uid'"
 );
 
+// Jika data ditemukan berarti user sudah like -> lakukan unlike
 if(mysqli_num_rows($cek)){
-    // unlike
+    // Menghapus data like dari database (unlike)
     mysqli_query($koneksi,
         "DELETE FROM likefoto WHERE FotoID='$foto' AND UserID='$uid'"
     );
 } else {
-    // beri like
+    // Jika belum pernah like -> tambahkan like
+    
+    // Mengambil tanggal hari ini
     $tgl = date("Y-m-d");
+    
+    // Menyimpan data like ke database
     mysqli_query($koneksi,
         "INSERT INTO likefoto VALUES('', '$foto', '$uid', '$tgl')"
     );
 }
 
+// Setelah proses selesai, kembali ke halaman detail foto
 header("Location: detail.php?id=$foto");
 ?>
