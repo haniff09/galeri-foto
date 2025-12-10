5deb7267071f0325054e64c99d036b0843cd9691
haniff09 <qowiyhanif@gmail.com>
2025-12-10 08:24:43 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <?php
+// Memulai session agar bisa membaca data login user
 session_start();
+
+// Mengecek apakah session UserID sudah ada (user sudah login)
 if(isset($_SESSION['UserID'])){
+    // Jika sudah login, arahkan ke halaman home
     header("Location: home.php");
 } else {
+    // Jika belum login, arahkan ke halaman login
     header("Location: login.php");
 }
 ?>
