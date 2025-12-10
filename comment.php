613236847bfd425815895a572751b1a2ab361b30
haniff09 <qowiyhanif@gmail.com>
2025-12-10 08:20:17 +0700

    Update comment.php

--- comment.php
+++ comment.php
@@ -1,15 +1,18 @@
 <?php
-session_start();
-include "config.php";
+session_start(); // Memulai session agar data user bisa diakses
+include "config.php"; // Menghubungkan ke database
 
-$foto = $_POST['fotoid'];
-$isi  = $_POST['isi'];
-$uid  = $_SESSION['UserID'];
-$tgl  = date("Y-m-d");
+// Mengambil data dari form
+$foto = $_POST['fotoid']; // ID foto yang dikomentari
+$isi  = $_POST['isi'];    // Isi komentar dari user
+$uid  = $_SESSION['UserID']; // ID user yang sedang login
+$tgl  = date("Y-m-d"); // Mengambil tanggal hari ini
 
+// Menyimpan komentar ke dalam tabel komentarfoto
 mysqli_query($koneksi,
     "INSERT INTO komentarfoto VALUES('', '$foto', '$uid', '$isi', '$tgl')"
 );
 
+// Setelah komentar disimpan, user diarahkan kembali ke halaman detail foto
 header("Location: detail.php?id=$foto");
 ?>
